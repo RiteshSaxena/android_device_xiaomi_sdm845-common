be18317565b61c7ea2a4ee8394a0bb3caaa7388c
TheScarastic <warabhishek@gmail.com>
2018-10-13 09:24:27 -0400

    sdm845-common: Force-enable volte
    For some reason volte wasnt working on carriers even with
    updated carrierconfig overlays so add it for now
    
    Change-Id: Iad5523bb4a0637912a4674e870b6eab5bf13a6ca

--- common-props.mk
+++ common-props.mk
@@ -52,6 +52,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # RCS and IMS
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
     persist.data.iwlan.enable=true \
     persist.rcs.supported=0 \
     persist.vendor.ims.disableUserAgent=0
