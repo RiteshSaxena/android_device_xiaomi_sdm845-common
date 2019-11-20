f80f59a947c642a5d7f79bf73a4549902309f716
Ritesh Saxena <riteshsax007@gmail.com>
2019-11-20 10:58:21 +0000

    sdm845-common: Build SnapdragonCamera
    Change-Id: Ib5a709bbb69e64462cdb1ea047c150a3edf4cd6c

--- sdm845.mk
+++ sdm845.mk
@@ -112,6 +112,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0.vendor
 
+PRODUCT_PACKAGES += \
+    SnapdragonCamera
+
 # Codec2 modules
 PRODUCT_PACKAGES += \
     com.android.media.swcodec \
