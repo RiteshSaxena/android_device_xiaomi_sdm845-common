8c8e14601cf0f03e0ffd650831b7c088ceacd3b6
Ritesh Saxena <riteshsax007@gmail.com>
2019-11-21 08:04:49 +0000

    Revert "sdm845-common: Build SnapdragonCamera"
    This reverts commit f80f59a947c642a5d7f79bf73a4549902309f716.

--- sdm845.mk
+++ sdm845.mk
@@ -114,9 +114,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0.vendor
 
-PRODUCT_PACKAGES += \
-    SnapdragonCamera
-
 # Codec2 modules
 PRODUCT_PACKAGES += \
     com.android.media.swcodec \
