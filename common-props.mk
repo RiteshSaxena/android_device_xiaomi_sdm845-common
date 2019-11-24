fbd98d8cd88d8f293ebbc401b6db32927b3325d6
Ritesh Saxena <riteshsax007@gmail.com>
2019-11-24 17:55:22 +0000

    Revert "sdm845-common: Enable framework ZSL"
    This reverts commit 99859889502620968f1461fff264c60ee102b51b.

--- common-props.mk
+++ common-props.mk
@@ -97,6 +97,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
+    camera.disable_zsl_mode=true \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera
 
 # Codec2 switch
